4dcd29068db3e09ed258099f0f186d64bd93c315
Bright Amofa Mintah <mintahb642@gmail.com>
2026-03-18 13:02:39 +0000

    Initial Spring Boot project

---
+++ src/main/java/com/brightamofa/SoftwareEngineerController.java
@@ -0,0 +1,26 @@
+package com.brightamofa;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
+    @GetMapping("{id}")
+    public SoftwareEngineer getEngineers(@PathVariable Integer id){
+        return softwareEngineerService.getSoftwareEngineerById(id);
+    }
+
+    @PostMapping
+    public void addNewSoftwareEngineer(@RequestBody SoftwareEngineer softwareEngineer){
+        softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
+    }
+}
